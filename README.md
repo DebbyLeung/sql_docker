e324862c0bd238d993a8778abea4f27396788bd2
DebbyLeung <stebbyleung@gmail.com>
2023-10-23 18:07:26 +0800

    commit update readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # sql_docker
+
+## venv
+```
+source venv/bin/activate
+pip install -r requirements.txt
+```
+## docker image
 ```
 cd ~/{path_to_repo}/sql_docker
 docker-compose up -d
--- scripts/sql_pyscript.py
+++ scripts/sql_pyscript.py
@@ -5,20 +5,26 @@ import pathlib
 import pandas as pd
 from icecream import ic
 parent_dir = pathlib.Path(__file__).parent.parent
-json_dir = parent_dir.joinpath('json/user_info.json')
+def init():
+    json_dir = parent_dir.joinpath('json/user_info.json')
+    with json_dir.open() as f:
+        users_dict  = json.load(f)
+        user_dict = users_dict["root"]
 
-with json_dir.open() as f:
-    users_dict  = json.load(f)
-    user_dict = users_dict["root"]
+    db = mysql.connector.connect(**user_dict)
+    return db
+def cursor_obj(query):
+    cursor = db.cursor(dictionary=True)
+    cursor.execute(query)
+    dataset = cursor.fetchall()
+    df = pd.DataFrame(dataset)
+    return df
 
-db = mysql.connector.connect(**user_dict)
-cursor = db.cursor(dictionary=True)
-stmt_select = "SELECT * FROM {0}".format("authors")
-cursor.execute(stmt_select)
-db.close()
-
-dataset = cursor.fetchall()
-
-df = pd.DataFrame(dataset )#, columns=columns)
-print(df)
-
+if __name__ == "__main__":
+    try:
+        db = init()
+        query = "SELECT * FROM {0}".format("authors")
+        df = cursor_obj(query)
+        ic(df)
+    finally:
+        db.close()
